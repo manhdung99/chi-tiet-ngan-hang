be379785349b897f3980f1a165fd36cbf1dfef52
manhdung99it <manhdung99it@gmail.com>
2024-01-24 16:51:57 +0700

    update save button color

--- src/components/popup/selectQuestionFromBank.vue
+++ src/components/popup/selectQuestionFromBank.vue
@@ -117,7 +117,7 @@
         </button>
         <button
           @click="saveData"
-          class="text-white text-sm bg-blue-400 px-4 py-2 rounded ml-1 hover:opacity-80"
+          class="text-white text-sm bg-blue-700 px-4 py-2 rounded ml-1 hover:opacity-80"
         >
           Lưu
         </button>
